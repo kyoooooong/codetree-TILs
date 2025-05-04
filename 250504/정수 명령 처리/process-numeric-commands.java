e69d12d58b9ac1c6ca405ec3857a7d2e6c16d3b5
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-05-04 11:39:20 +0900

    Title: 정수 명령 처리, Time: 349ms, Memory: 20MB, Status: Passed - Codetree

---
+++ 250504/정수 명령 처리/process-numeric-commands.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            String command = sc.nextLine();
+
+            if (command.startsWith("push")) {
+                int value = Integer.parseInt(command.split(" ")[1]);
+                stack.push(value);
+            } else if (command.equals("pop")) {
+                if (stack.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(stack.pop());
+                }
+            } else if (command.equals("size")) {
+                System.out.println(stack.size());
+            } else if (command.equals("empty")) {
+                System.out.println(stack.isEmpty() ? 1 : 0);
+            } else if (command.equals("top")) {
+                if (stack.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(stack.peek());
+                }
+            }
+        }
+        sc.close();
+    }
+}
