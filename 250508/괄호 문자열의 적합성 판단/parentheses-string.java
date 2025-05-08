7e933d5c56ac345296e354c4955d80260f5e1046
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-05-08 18:00:13 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 250508/괄호 문자열의 적합성 판단/parentheses-string.java
+++ 250508/괄호 문자열의 적합성 판단/parentheses-string.java
@@ -2,30 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
-
         Scanner sc = new Scanner(System.in);
-
         String s = sc.nextLine();
-        int total =0;
+        int total = 0;
 
-        for(char c:s.toCharArray()) {
-
-            if(c=='(') {
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
                 total++;
-            } else if(c==')') {
+            } else if (c == ')') {
                 total--;
             }
-            if(total <0) {
+
+            if (total < 0) {
+                System.out.println("No");  // 빠르게 탈출할 땐 출력도 해줘야 함
                 return;
             }
         }
-        if(total==0) {
+
+        if (total == 0) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
         }
-
-
     }
 }
