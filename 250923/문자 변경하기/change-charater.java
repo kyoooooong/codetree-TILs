403bdfdcfe3a6b659b9b6b8912870a2fcac9c34b
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-09-23 09:22:03 +0900

    Title: 문자 변경하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250923/문자 변경하기/change-charater.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        char a = 'C';
+        a = 'T';
+
+        system.out.println(a);
+        // Please write your code here.
+    }
+}
