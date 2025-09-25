32d38ccbef4e8cf06be5fafae5cc03e7fedd8841
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-09-25 09:37:10 +0900

    Title: 합과 평균의 차, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250925/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int d =a+b+c;
+        int e = d/3;
+
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(d-e);
+    }
+}
