d9d5e4d3e787583524518e71f40f0d1ff64d5ae2
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-09-26 09:37:05 +0900

    Title: 물의 상태, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 250926/물의 상태/state-of-water.java
+++ 250926/물의 상태/state-of-water.java
@@ -7,9 +7,9 @@ public class Main {
 
         if (a<0) {
             System.out.print("ice");
-        } else if (a>100) {
+        } else if (a>=100) {
             System.out.print("vapor");
-        } else if (a>=0 && a<=100) {
+        } else if (a>=0 && a<100) {
             System.out.print("water");
         }
     }
