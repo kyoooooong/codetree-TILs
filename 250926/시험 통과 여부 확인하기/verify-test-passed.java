d43336f65ff6a3ec0da51e1f723c102d88dddb0e
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-09-26 09:24:10 +0900

    Title: 시험 통과 여부 확인하기, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250926/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if (a>=80) {
+            System.out.println("pass");
+        } else {
+            System.out.printf("%d more score", (80-a));
+        }
+    }
+}
