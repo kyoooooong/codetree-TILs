6ca4ecb8cb29f82ad215b8c508841b9e2b460ae0
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-10-10 16:30:26 +0900

    Title: 별표 출력하기 2, Time: 110ms, Memory: 10MB, Status: Passed - Codetree

--- 251010/별표 출력하기 2/print-star-2.java
+++ 251010/별표 출력하기 2/print-star-2.java
@@ -4,12 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        int b = a;
 
         for(int i=0;i<a;i++) {
-            for( int j=0;j<a;j++) {
+            for( int j=0;j<b;j++) {
                 System.out.print("*"+" ");
             }
             System.out.print("\n");
+            b--;
         }
 
 
