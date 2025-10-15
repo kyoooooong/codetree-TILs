b8654e21697b0482cadf80b227bb623c7fdf5561
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-10-15 09:25:55 +0900

    Title: 특정 원소들의 합, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251015/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = new int[4][4];
+        int num;
+        int total = 0;
+
+        // 첫 번째 배열 입력
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                num= sc.nextInt();
+                if (j<=i) {
+                    total += num;
+                }
+            }
+        }
+        System.out.println(total);
+    }
+}
