de02a9691c6810335621de57d6e29ffddc401f4f
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-10-18 16:25:27 +0900

    Title: 실수 입력받아 계산 2, Time: 141ms, Memory: 11MB, Status: Passed - Codetree

--- 251018/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
+++ 251018/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double n = sc.nextDouble();
-        double a = n*1.5;
+        double a = n+1.5;
 
         System.out.printf("%.2f", a);
     }
