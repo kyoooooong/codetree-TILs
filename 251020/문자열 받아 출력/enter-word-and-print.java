7aa24af33d98566d3a307210f6253ef9f4ddad87
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-10-20 14:12:25 +0900

    Title: 문자열 받아 출력, Time: 117ms, Memory: 10MB, Status: Passed - Codetree

--- 251020/문자열 받아 출력/enter-word-and-print.java
+++ 251020/문자열 받아 출력/enter-word-and-print.java
@@ -4,11 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String m = sc.next();
-        char n = m.charAt(0);
 
         System.out.print(m);
 
-    }
-}
     }
 }
