364cdbdcf38396b8464612b492b6f6ad69b3b427
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-10-20 14:05:52 +0900

    Title: 문자 받아 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251020/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char n = sc.next().CharAt(0);
+
+        System.out.print(n);
+
+    }
+}
