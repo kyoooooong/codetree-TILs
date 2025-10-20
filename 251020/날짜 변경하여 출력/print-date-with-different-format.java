2398362648ea9f4b4248b98bb9fa220839b333bb
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-10-20 14:28:11 +0900

    Title: 날짜 변경하여 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251020/날짜 변경하여 출력/print-date-with-different-format.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String m = sc.next();
+        String[] arr = m.split("-");
+        System.out.print(arr[1]+"-"+arr[2]+"-"+arr[0]);
+    }
+}
+    }
+}
