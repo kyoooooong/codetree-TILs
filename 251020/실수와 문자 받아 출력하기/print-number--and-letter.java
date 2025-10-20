17198556fe61504505f8b3d898fcaf19297c4d20
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-10-20 14:15:04 +0900

    Title: 실수와 문자 받아 출력하기, Time: 150ms, Memory: 11MB, Status: Passed - Codetree

--- 251020/실수와 문자 받아 출력하기/print-number--and-letter.java
+++ 251020/실수와 문자 받아 출력하기/print-number--and-letter.java
@@ -10,7 +10,7 @@ public class Main {
         double b = sc.nextDouble();
 
         System.out.println(n);
-        System.out.printf("%.2f",a);
+        System.out.printf("%.2f\n",a);
         System.out.printf("%.2f",b);
     }
 }
