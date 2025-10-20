f633d27a1b8a08d0e89df434c0fa5a5e64744ec4
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-10-20 14:30:45 +0900

    Title: 날짜 변경하여 출력, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

--- 251020/날짜 변경하여 출력/print-date-with-different-format.java
+++ 251020/날짜 변경하여 출력/print-date-with-different-format.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String m = sc.next();
-        String[] arr = m.split(".");
+        String[] arr = m.split("\\.");
         System.out.print(arr[1]+"-"+arr[2]+"-"+arr[0]);
     }
 }
-    
