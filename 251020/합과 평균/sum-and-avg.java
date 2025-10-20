1d93f1f42b5c9bcd015f8ec86b9bd418f31718df
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-10-20 15:00:46 +0900

    Title: 합과 평균, Time: 150ms, Memory: 10MB, Status: Passed - Codetree

--- 251020/합과 평균/sum-and-avg.java
+++ 251020/합과 평균/sum-and-avg.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        System.out.printf("%d %.2f",a+b,(double)(a+b)/2);
+        System.out.printf("%d %.1f",a+b,(double)(a+b)/2);
 
 
     }
