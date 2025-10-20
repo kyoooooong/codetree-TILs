612a3bbe7b48554cb9188694168a7de7457a3b20
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-10-20 14:25:39 +0900

    Title: 입력받아 출력 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251020/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String m = sc.next();
+        String[] arr = m.split("-");
+        System.out.print(arr[0],arr[1]);
+    }
+}
