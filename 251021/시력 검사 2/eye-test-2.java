7be31b1b16abe0884544da9a733787e7c3d7350f
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-10-21 09:40:14 +0900

    Title: 시력 검사 2, Time: 136ms, Memory: 11MB, Status: Passed - Codetree

--- 251021/시력 검사 2/eye-test-2.java
+++ 251021/시력 검사 2/eye-test-2.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble();
-        if(a>1.0) {
+        if(a>=1.0) {
             System.out.println("High");
-        } else if (a>0.5) {
+        } else if (a>=0.5) {
             System.out.println("Middle");
         } else {
             System.out.println("Low");
