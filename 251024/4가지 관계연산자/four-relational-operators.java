6695655562ddc5aae131f49cacf4c5fb464d771d
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-10-24 11:34:04 +0900

    Title: 4가지 관계연산자, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251024/4가지 관계연산자/four-relational-operators.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.printf("%d \n", (A >= B) ? 1 : 0);
+        System.out.printf("%d \n", (A > B) ? 1 : 0);
+        System.out.printf("%d \n", (B >= A) ? 1 : 0);
+        System.out.printf("%d \n", (B > A) ? 1 : 0);
+    }
+}
