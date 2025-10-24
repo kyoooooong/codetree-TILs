595900d83d097d4d46aefcbc85a5bfd68f0a78ef
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-10-24 11:57:19 +0900

    Title: 세 정수의 최솟값, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251024/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int c = sc.nextInt();
+
+        if(A<=B && A<=c) {
+            System.out.print(A);
+        } else if(B<=A && B<=c) {
+            System.out.print(B);
+        } else {
+            System.out.print(C);
+        }
+
+    }
+}
