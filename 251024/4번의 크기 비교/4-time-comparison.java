5e9a9cbff3bce8f45d69ab01b0afeef98eb6f9dd
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-10-24 11:35:25 +0900

    Title: 4번의 크기 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251024/4번의 크기 비교/4-time-comparison.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+
+        System.out.printf("%d \n", (A > B) ? 1 : 0);
+        System.out.printf("%d \n", (A > c) ? 1 : 0);
+        System.out.printf("%d \n", (A > d)) ? 1 : 0);
+        System.out.printf("%d \n", (A > e) ? 1 : 0);
+    }
+}
