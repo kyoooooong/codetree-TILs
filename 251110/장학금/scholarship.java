60cd730ab4453cc85099dd803f59c0a355ac05fa
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-11-10 13:57:28 +0900

    Title: 장학금, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 251110/장학금/scholarship.java
+++ 251110/장학금/scholarship.java
@@ -7,9 +7,9 @@ public class Main {
         int a = sc.nextInt();
         if (b>=90) {
             if(a>=95 && a<=100) {
-            System.out.print("10");
+            System.out.print("100000");
         } else if(a>=90) {
-            System.out.print("5");
+            System.out.print("50000");
         } else {
             System.out.print("0");
         }
