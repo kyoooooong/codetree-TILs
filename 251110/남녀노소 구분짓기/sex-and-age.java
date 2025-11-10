c283edc4224c56d448674a16eba8bded3bf3611f
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-11-10 14:08:59 +0900

    Title: 남녀노소 구분짓기, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251110/남녀노소 구분짓기/sex-and-age.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a==0) {
+            if (c>=19) {
+                System.out.print("MAN");
+            } else {
+                System.out.print("BOY");
+            }
+           
+        } else  {
+            if (c>=19) {
+                System.out.print("WOMAN");
+            } else {
+                System.out.print("GIRL");
+            }
+        }
+    }
+}
