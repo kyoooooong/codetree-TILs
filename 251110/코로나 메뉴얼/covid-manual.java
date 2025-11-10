8848610bcea9218799b99a18335adce8d4053e3e
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-11-10 23:02:06 +0900

    Title: 코로나 메뉴얼, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 251110/코로나 메뉴얼/covid-manual.java
+++ 251110/코로나 메뉴얼/covid-manual.java
@@ -3,33 +3,21 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int cnt =0;
-        for (int i=0;i<3;i++) {
+        int cnt = 0;
+
+        for (int i = 0; i < 3; i++) {
             char a = sc.next().charAt(0);
-        int b = sc.nextInt();
+            int b = sc.nextInt();
 
-        if (a=="Y"){
-            if(b>=37) {
-                System.out.print("");
-                cnt ++;
-            } else {
-                System.out.print("");
+            if (a == 'Y' && b >= 37) {
+                cnt++;
             }
-        } else  {
+        }
 
-                if(b>=37) {
-                System.out.print("");
-            } else {
-                System.out.print("");
-            }
-            
+        if (cnt >= 2) {
+            System.out.print("E");
+        } else {
+            System.out.print("N");
         }
-        if(cnt>=2) {
-                System.out.print("E");
-                break;
-            } 
-        }
-        System.out.print("N");
-        
     }
 }
