1dab57f042f32850f49b736b8812e7801371083e
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-11-10 23:19:42 +0900

    Title: 증가시키며 출력하기 2, Time: 93ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 251110/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int cnt=0;
+        while(cnt<26) {
+            cnt++;
+            System.out.print(cnt);
+        }
+    }
+}
