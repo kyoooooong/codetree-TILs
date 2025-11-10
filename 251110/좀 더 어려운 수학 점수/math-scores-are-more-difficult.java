52731f1dd0fa239db8be2d038682ebc247138e9a
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-11-10 14:01:00 +0900

    Title: 좀 더 어려운 수학 점수, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251110/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        if (a > c) {
+            System.out.print("A");
+           
+        } else if (c>a) {
+            System.out.print("B");
+    } else if (c==a) {
+            if (b>d) {
+            System.out.print("A");
+    } else {
+            System.out.print("B");
+    }
+    }
+}}
