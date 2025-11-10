b1f7773a3f5e7135999eddf793a7817d019b165a
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-11-10 14:03:34 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251110/두 사람/two-person.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        char b = sc.next().chatAt(0);
+
+        int c = sc.nextInt();
+        char d = sc.next().chatAt(0);
+
+        if ((a>=19 || c>=19)&&(b=='M'||d=='M')) {
+            System.out.print("1");
+           
+        } else  System.out.print("0");
+    }
+}
