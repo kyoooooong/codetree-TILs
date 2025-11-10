28d8a8db0d8d84f464fdb33e25ca28ad2e7edecb
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-11-10 14:04:29 +0900

    Title: 두 사람, Time: 121ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251110/두 사람/two-person.java
+++ 251110/두 사람/two-person.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        char b = sc.next().chatAt(0);
+        char b = sc.next().charAt(0);
 
         int c = sc.nextInt();
-        char d = sc.next().chatAt(0);
+        char d = sc.next().charAt(0);
 
         if ((a>=19 || c>=19)&&(b=='M'||d=='M')) {
             System.out.print("1");
