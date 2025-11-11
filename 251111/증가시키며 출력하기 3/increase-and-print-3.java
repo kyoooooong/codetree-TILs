daca629a75c639c1dc365bdacbf3dabc4b18d58b
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-11-11 21:59:21 +0900

    Title: 증가시키며 출력하기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251111/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int cnt=9;
+
+        while(cnt<26) {
+            cnt++=2;
+            
+            System.out.printf("%d ", cnt);
+        }
+    }
+}
