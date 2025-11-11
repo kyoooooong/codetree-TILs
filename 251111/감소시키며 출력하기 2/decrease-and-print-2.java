40150d14d074908335ad8046f058ba9fe0e7e121
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-11-11 22:11:51 +0900

    Title: 감소시키며 출력하기 2, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251111/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        int cnt =26;
+        while (cnt>=10) {
+            System.out.printf("%d ", cnt);
+            cnt--;
+        }
+    }
+}
