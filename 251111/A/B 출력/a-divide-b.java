e3ce239d5fff215c77704f2949a17c4bd2f45b1e
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-11-11 22:32:03 +0900

    Title: A/B 출력, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251111/A/B 출력/a-divide-b.java
+++ 251111/A/B 출력/a-divide-b.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int a = sc.nextInt();   // 첫 번째 정수 입력
+        int b = sc.nextInt();   // 두 번째 정수 입력
 
-        System.out.printf("%.21f",(double)a/b);
+        System.out.printf("%.21f", (double)a / b); // 실수 나눗셈 후 소수점 21자리 출력
     }
 }
