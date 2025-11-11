cf526330ab309d1bcf86f28466fb5120d467cf86
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-11-11 22:01:02 +0900

    Title: 증가시키며 출력하기 3, Time: 124ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 251111/증가시키며 출력하기 3/increase-and-print-3.java
+++ 251111/증가시키며 출력하기 3/increase-and-print-3.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int cnt=9;
+        int cnt=8;
 
         while(cnt<26) {
             cnt+=2;
