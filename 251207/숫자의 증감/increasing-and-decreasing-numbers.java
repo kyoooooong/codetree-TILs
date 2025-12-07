36cb81540f34fa9846dd87c2761de30f1fc3e86b
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-12-07 21:21:25 +0900

    Title: 숫자의 증감, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251207/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next().charAt(0);   // 첫 번째 정수 입력
+        int b = sc.nextInt();   // 두 번째 정수 입력
+
+        if (a=='A') {
+            for(int i=1;i<=b;i++) {
+                System.out.printf("%d ",i);
+            }
+
+        } else {
+            for(int i=b;i>=1;i--) {
+                System.out.printf("%d ",i);
+            }
+        }
+    }
+}
