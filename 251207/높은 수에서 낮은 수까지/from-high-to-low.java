fe6e3ff7e15954cbd0e446cd1e8b08db7f2a3d21
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-12-07 21:39:02 +0900

    Title: 높은 수에서 낮은 수까지, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251207/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();  // ✅ char 타입으로 수정
+        int b = sc.nextInt();
+
+        if (a>=b) {
+            for(int i=b;i<=a;i++) {
+                System.out.printf("%d ", i);
+            }
+        } else {
+            for(int i=a;i<=b;i++) {
+                System.out.printf("%d ", i);
+            }
+        }
+    }
+}
