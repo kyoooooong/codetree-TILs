fb6688550aee01a12ac08ef321a3055873a78cde
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-12-08 23:09:06 +0900

    Title: a의 배수, Time: 3050ms, Memory: 25MB, Status: Time Limit Exceed - Codetree

---
+++ 251208/a의 배수/multiple-of-a.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int c = 0;
+
+        while (c<=b) {
+            if (c%b==0) {
+                System.out.print(1);
+            } else {
+                System.out.print(0);
+            }
+        }
+    }
+}
